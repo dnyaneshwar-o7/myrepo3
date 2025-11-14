db9543fed49f72ef2303d7881efeec61e470215b
dnyaneshwar-o7 <134317848+dnyaneshwar-o7@users.noreply.github.com>
2025-11-14 19:45:59 +0530

    Add files via upload

---
+++ ProjectWithMaven/src/main/java/org/example/Alien.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Alien {
+
+    private int age;
+ private String aname;
+ private String teach;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTeach() {
+        return teach;
+    }
+
+    public void setTeach(String teach) {
+        this.teach = teach;
+    }
+}
---
+++ ProjectWithMaven/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.apache.hadoop.shaded.org.eclipse.jetty.websocket.common.SessionFactory;
+import org.sparkproject.jetty.server.session.Session;
+
+import java.lang.module.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       Alien a1 = new Alien();
+       a1.setAge(101);
+       a1.setAname("navin");
+       a1.setTeach("java");
+
+    }
+
+}
